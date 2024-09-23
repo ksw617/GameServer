4d5ffc96d01f1d77021e406550e1e4694d79dbb9
lecture <ksw617@gmail.com>
2024-09-23 17:54:09 +0900

    update

--- GameClient/GameClient.cpp
+++ GameClient/GameClient.cpp
@@ -1,13 +1,71 @@
-// GameClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+#pragma comment(lib, "Ws2_32.lib")
+#include <WinSock2.h>	
+#include <WS2tcpip.h> 
 
 int main()
-{
-    std::cout << "Hello This is Client!\n";
-	while (true)
-	{
+{               
+    printf("============== Client  ================\n");
 
-	}
+    WORD wVersionRequested;
+    WSADATA wsaData;
+
+    wVersionRequested = MAKEWORD(2, 2);
+
+    if (WSAStartup(wVersionRequested, &wsaData) != 0)
+    {
+        printf("WSAStarup filed with error\n");
+        return 1;
+    }
+
+    //소켓 만들기
+    SOCKET connectSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (connectSocket == INVALID_SOCKET)
+    {
+        printf("socket function failed with error : %d\n", WSAGetLastError());
+    }
+    else
+    {
+        printf("socket function succeded\n");
+    }
+
+    //서버의 주소
+    SOCKADDR_IN service;
+    memset(&service, 0, sizeof(service));
+    service.sin_family = AF_INET;
+    inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
+    service.sin_port = htons(27015);
+
+	//TODO
+
+    //서버에 접속 service(접속할 서버 정보)
+    //connect(내 소켓, 서버의 주소, 해당 주소의 구조체 크기)
+    if (connect(connectSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
+    {
+        //에러 코드 확인
+        printf("connect function failed with error : %d\n", WSAGetLastError());
+        //소켓 닫기
+        closesocket(connectSocket);
+        //winsock dll 사용 종료
+        WSACleanup();
+
+        //프로그램 종료
+        return 1;
+
+    }
+
+    printf("Connect to Server\n");
+
+    while (true)
+    {
+        //TODO
+        Sleep(1000);
+    }
+
+  
+    closesocket(connectSocket);
+
+    WSACleanup();
 }
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,13 +1,76 @@
-// GameServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+#pragma comment(lib, "Ws2_32.lib")
+#include <WinSock2.h>	
+#include <WS2tcpip.h> 
 
 int main()
 {
-    std::cout << "Hello this is Server!\n";
-	while (true)
-	{
+    printf("============== Server  ================\n");
+    WORD wVersionRequested;
+    WSADATA wsaData;
 
-	}
+    wVersionRequested = MAKEWORD(2,2);
+            
+    if (WSAStartup(wVersionRequested, &wsaData) != 0)
+    {
+        printf("WSAStarup filed with error\n");
+        return 1;
+    }
+
+
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket == INVALID_SOCKET)
+    {                  
+        printf("socket function failed with error : %d\n", WSAGetLastError());
+    }
+    else  
+    {
+        printf("socket function succeded\n");
+    }
+
+    SOCKADDR_IN service;
+    memset(&service, 0, sizeof(service));
+    service.sin_family = AF_INET;
+    inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
+    service.sin_port = htons(27015);
+
+    if (bind(listenSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
+    {
+        printf("bind failed with error : %d\n", WSAGetLastError());
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+
+
+    }
+
+    if (listen(listenSocket, 10) == SOCKET_ERROR)
+    {
+        printf("listen failed with error : %d\n", WSAGetLastError());
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    while (true)
+    {
+        printf("listening...\n");
+
+       SOCKET acceptSocket = accept(listenSocket, NULL, NULL); 
+
+       if (acceptSocket == INVALID_SOCKET)
+       {
+           printf("accept failed with error : %d\n", WSAGetLastError());
+           continue;
+       }
+
+        printf("Client Connected!!\n");
+    }
+  
+    closesocket(listenSocket);
+ 
+    WSACleanup();
+    
 }
