f7acbafba014ae89aa00d08bd543bc0e817af5a8
lecture <ksw617@gmail.com>
2024-10-01 18:02:31 +0900

    Update GameClient.cpp

--- GameClient/GameClient.cpp
+++ GameClient/GameClient.cpp
@@ -4,65 +4,31 @@ using namespace std;
 #pragma comment(lib, "Ws2_32.lib")
 #include <WinSock2.h>	
 #include <WS2tcpip.h> 
+#include <MSWSock.h> // 추가
 
 #include <thread> 
 
-enum IO_TYPE
+//비동기 연결 완료를 대기하고 결과를 처리하는 스레드 함수
+void ConnectThread(HANDLE iocpHandle)
 {
-    NONE,
-    SEND,
-    RECV,
-};
-
-struct Session
-{
-    WSAOVERLAPPED overlapped = {};  
-    SOCKET socket = INVALID_SOCKET; 
-    IO_TYPE type;
-    char recvBuffer[512] = {};
-    char sendBuffer[512] = {};    
-
-};
-
-void SendRecvThread(HANDLE iocpHandle)
-{
-    DWORD byteTransferred = 0;
+    DWORD bytesTransferred = 0;
     ULONG_PTR key = 0;
-    Session* session = nullptr;
+    WSAOVERLAPPED overlapped = {};
 
+    printf("Waiting....\n");
+
+    //대기
+    if (GetQueuedCompletionStatus(iocpHandle, &bytesTransferred, &key, (LPOVERLAPPED*)&overlapped, INFINITE)) 
+    {
+        printf("Connect successed\n"); // 연결 성공 메세지 출력
+    }
+
+    //안꺼지게 1초에 한번씩 돌림
     while (true)
     {
-        printf("Waiting....\n");
-
-        GetQueuedCompletionStatus(iocpHandle, &byteTransferred, &key, (LPOVERLAPPED*)&session, INFINITE);
-
-        WSABUF wsaBuf;
-        wsaBuf.buf = session->recvBuffer;
-        wsaBuf.len = sizeof(session->recvBuffer);
-
-        DWORD bufferLen = 0;
-        DWORD flags = 0;
-
-        switch (session->type)
-        {
-        case SEND:
-            session->type = RECV;
-            WSARecv(session->socket, OUT & wsaBuf, 1, OUT & bufferLen, &flags, &session->overlapped, NULL);
-            break;
-        case RECV:
-            printf("Recv : %s\n", session->recvBuffer);
-
-            this_thread::sleep_for(1s);
-
-            session->type = SEND;
-            WSASend(session->socket, &wsaBuf, 1, &bufferLen, flags, &session->overlapped, NULL);
-            break;
-        default:
-            break;
-        }
-
-
+        this_thread::sleep_for(1s);
     }
+    
 }
 
 int main()
@@ -80,63 +46,73 @@ int main()
         return 1;
     }
 
-    SOCKET connectSocket = socket(AF_INET, SOCK_STREAM, 0);
+    SOCKET connectSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+
     if (connectSocket == INVALID_SOCKET)
     {
-        printf("socket function failed with error : %d\n", WSAGetLastError());
+        printf("WSASocket filed with error : %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
     }
-    else
-    {
-        printf("socket function succeded\n");
-    }
-             
-    SOCKADDR_IN service;
-    memset(&service, 0, sizeof(service));
-    service.sin_family = AF_INET;
-    inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
-    service.sin_port = htons(27015);
 
-    if (connect(connectSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
+    DWORD dwBytes;
+    LPFN_CONNECTEX lpfnConnectEx = NULL;
+    GUID guidConnectEx = WSAID_CONNECTEX;
+    if (WSAIoctl(connectSocket, SIO_GET_EXTENSION_FUNCTION_POINTER, &guidConnectEx, sizeof(guidConnectEx),
+       &lpfnConnectEx, sizeof(lpfnConnectEx), &dwBytes, NULL, NULL) == SOCKET_ERROR)
     {
-        printf("connect function failed with error : %d\n", WSAGetLastError());
+        printf("WSAIoctl filed with error : %d\n", WSAGetLastError());
         closesocket(connectSocket);
         WSACleanup();
         return 1;
 
     }
 
-    printf("Connect to Server\n");
+    SOCKADDR_IN serverService;
+    memset(&serverService, 0, sizeof(serverService));
+    serverService.sin_family = AF_INET;
+    inet_pton(AF_INET, "127.0.0.1", &serverService.sin_addr);
+    serverService.sin_port = htons(27015);
+
+    SOCKADDR_IN clientService;
+    memset(&clientService, 0, sizeof(clientService));
+    clientService.sin_family = AF_INET;
+    clientService.sin_addr.s_addr = htonl(INADDR_ANY); 
+    clientService.sin_port = htons(0);
+
+
+    if (bind(connectSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR)
+    {
+        printf("bind filed with error : %d\n", WSAGetLastError());
+        closesocket(connectSocket);
+        WSACleanup();
+        return 1;
+    }
 
     HANDLE iocpHandle = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, NULL);
-
-    thread t(SendRecvThread, iocpHandle);
-
     ULONG_PTR key = 0;
-
     CreateIoCompletionPort((HANDLE)connectSocket, iocpHandle, key, 0);
 
-    Session* session = new Session;
-    session->socket = connectSocket;
-    session->type = SEND;
-    char sendBuffer[512] = "Hello this is Client";
+    thread t(ConnectThread, iocpHandle);
 
-    memcpy(session->sendBuffer, sendBuffer, sizeof(sendBuffer));
+    DWORD bytesTransferred = 0;
+    WSAOVERLAPPED overlapped = {};
 
-
-    WSABUF wsaBuf;
-    wsaBuf.buf = session->sendBuffer;
-    wsaBuf.len = sizeof(session->sendBuffer);
-
-    DWORD recvLen = 0;
-    DWORD sendLen = 0;
-    DWORD flags = 0;
-
-    WSASend(connectSocket, &wsaBuf, 1, &sendLen, flags, &session->overlapped, NULL);
+    if (lpfnConnectEx(connectSocket, (SOCKADDR*)&serverService, sizeof(serverService), nullptr, 0, &bytesTransferred, &overlapped) == FALSE)
+    {
+        if (WSAGetLastError() != ERROR_IO_PENDING)
+        {
+            printf("ConnectEx filed with error : %d\n", WSAGetLastError());
+            closesocket(connectSocket);
+            WSACleanup();
+            return 1;
+        }
+           
+    }
 
     t.join();
 
     closesocket(connectSocket);
-
     WSACleanup();
 
     return 0;
